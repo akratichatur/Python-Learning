92a3387af79e281fadc5b48432b079937a6c01c4
akratichatur <37690086+akratichatur@users.noreply.github.com>
2018-10-14 21:56:42 +0530

    updated

--- third.py
+++ third.py
@@ -3,7 +3,7 @@ if x=='a' or x=='e' or x=='i' or x=='o' or x=='u' or x=='A' or x=='E' or x=='I' 
     print("Vowel")
 else:
     x=ord(x)
-    if x>=0 and x<=64:
+    if (x>=0 and x<=64) or (x>=91 and x<=96) or (x>=123 and x<=127):
         print("invalid")
     else:
         print("Consonant")
